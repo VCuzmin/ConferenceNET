10604dbd04b8d459d5bbd509c9150867eddb9e8b
Vasile Cuzmin <vcuzmin@totalsoft.ro>
2021-07-30 11:39:53 +0300

    conference .net api project

---
+++ src/Conference.Api/Swagger/SwaggerExtension.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Conference.Api.Swagger
+{
+    public static class SwaggerExtension
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>        
+        /// <returns></returns>
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new Microsoft.OpenApi.Models.OpenApiInfo
+                    {
+                        Title = "Conference Api",
+                        Version = "v1"
+                    }
+                );
+
+                c.CustomSchemaIds(type => type.ToString());
+            });
+
+            return services;
+        }
+    }
+}
