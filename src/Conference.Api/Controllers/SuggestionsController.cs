63f13e664199be2ba9ff781ad2f9706b3a82305e
Vasile Cuzmin <vcuzmin@totalsoft.ro>
2021-08-02 13:54:07 +0300

    layers folders

---
+++ src/Conference.Api/Controllers/SuggestionsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Conference.Api.Controllers
+{
+    [Route("api/suggestions")]
+    [ApiController]
+    public class SuggestionsController: ControllerBase
+    {
+    }
+}
